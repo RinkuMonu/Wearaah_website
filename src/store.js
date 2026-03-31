e0c0542026cf8f6bcefcf027b2b4c175ab7d51fe
ranjay kumar <ranjaykumar73.rk@gmail.com>
2026-03-31 17:52:03 +0530

    changes in address

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./features/Productlist/ProductSlice"; 
-import categorySlice from "./features/Category/categorySlice"; 
-import cartReducer from "./features/Cart/cartSlice"; 
+import cartReducer from "./features/Cart/cartSlice";
 export const store = configureStore({
   reducer: {
     products: productReducer,
-    category: categorySlice,
     cart: cartReducer,
   },
-}); 
+});
